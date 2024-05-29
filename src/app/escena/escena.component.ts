99f80aeaaa39678c24b8cfd9e16b8fa2c6695e2d
Fran Palmero <sweetydesigns@gmail.com>
2024-05-29 19:29:49 +0200

    Exercici 3 + Exercici 4
    Update html to escene + Add style.
    Add images + background color to steps.
    Add steps logic.
    Import Bootstrap  v.5.3.3 from CDN.
    Update body style.

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { iStep } from '../i-step';
+import { StepsService } from '../steps.service';
 
 @Component({
   selector: 'app-escena',
@@ -10,5 +11,22 @@ import { iStep } from '../i-step';
   styleUrl: './escena.component.scss'
 })
 export class EscenaComponent {
+  counter: number = 0;
   @Input() frasesArray: iStep[] = [];
+
+  onNext() {
+    if (this.counter != this.frasesArray.length - 1) {
+      this.counter++;
+    }
+  }
+
+  onPrevious() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+  }
+
+  changeStep(index: number) {
+    this.counter = index;
+  }
 }
--- src/app/steps.service.ts
+++ src/app/steps.service.ts
@@ -4,19 +4,26 @@ import { iStep } from './i-step';
 @Injectable({
   providedIn: 'root'
 })
+
 export class StepsService {
   private frasesArray: iStep[] = [
     {
       title: 'Dedica moltes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
+      img: '/assets/time_managment.svg',
+      bgcolor: '#4da1a8',
     },
     {
       title: 'Programa projectes propis',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
+      img: '/assets/programming.svg',
+      bgcolor: '#d3d4d9',
     },
     {
       title: 'Procura descansar',
       description: 'Descansar bé i desconnectar són vitals. D\'aquesta manera reduiràs l\'estrès i l\'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.',
+      img: '/assets/meditation.svg',
+      bgcolor: '#ffd167',
     },
   ];
 
